6e411e6a8e3c0a8677bb372468673364e1bb35f7
Bíborka <64154594+drq0tq@users.noreply.github.com>
2021-04-17 22:45:01 +0200

    adatbázis kapcsolat módosítása

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 //error_reporting(0);
-$db = new mysqli('127.0.0.1','root','','menhely');
+$db = new mysqli('127.0.0.1','root','','menhelydb');
 if($db->connect_errno){
 echo $db->connect_error;
 die();
