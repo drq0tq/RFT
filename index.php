01c4d9301c40eaf07c82b5d0e336699f6747f9b2
Bíborka <64154594+drq0tq@users.noreply.github.com>
2021-04-17 21:39:15 +0200

    index oldal létrehozása

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'connect.php';
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body >
+    <table id="bejelentkezes" align="right" >
+	  <tr><td><input type="button" value="Bejelentkezés adminként" onclick="location='bejelentkezes.html';"></td></tr>
+	  </table>
+<div id=Fejlec > </div>
+	
+<div class="navbar">
+  <a href="rolunk.php">Rólunk</a>
+  <a href="allatok.php">Örökbefogadható állataink</a>
+  <a href="talaltallat.php">Talált állat bejelentése</a>
+  <a href="gondozok.php">Gondozóink</a>  
+    <a href="tamogatas.php">Támogatás</a>
+    <a href="orokbefogadott.php">Örökbefogadott kutyák</a>
+  <a href="kapcsolat.php">Kapcsolat</a>
+</div>
+
+</body>
+</html>
