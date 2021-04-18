6c66f36b7c5eadfdffbfe6c7afff708fe25d4585
Bíborka <64154594+drq0tq@users.noreply.github.com>
2021-04-18 14:08:08 +0200

    támogatás oldak style

---
+++ tamogatas.php
@@ -0,0 +1,114 @@
+<?php
+include 'index.php';
+?>
+<html>
+<head>
+    <style>
+    h1{
+        margin-top: 53px;
+    }
+   body
+    {
+      
+       color:#e65c00;
+       
+        
+    }
+    p{
+        font-size: 20px;
+    }
+    form {
+        width: 70%;
+        padding: 20px 50px 20px 20px;
+        margin-bottom: auto;
+        margin-left: auto;
+        margin-right: auto;
+         font-family: cursive;
+        background-color:#ffdd99;
+        
+        
+    }
+</style>
+</head>
+<body>
+<form align="center">
+  <h1 > Hogyan segíthet?</h1> 
+    <h2>Pénzadománnyal</h2> 
+    <p>Egyesületünk kizárólag adományokból, felajánlásokból tartja fent magát, így fix havi bevétellel nem rendelkezünk, amely folyamatosan biztosítaná a működéshez szükséges anyagiakat. Nagy segítség, ha bármilyen kis összeggel is, de havonta tud támogatni bennünket! </p>
+<h2>Tagsággal</h2> <img src="kutya1.jpg"  width="30%" align="right" >
+
+<p>Ha egyetért céljainkkal, törekvéseinkkel, akkor csatlakozzon hozzánk és legyen tagunk!
+Tagsági nyilatkozat letölthető innen <br/><br/>
+
+Tagsági díjak:<br/><br/>
+
+    </p><ul>
+<li>Felnőtt: 3000 Ft/év
+<li> Diák 900 Ft/év
+<li> Nyugdíjas 1800 Ft/év
+</ul> 
+   
+   <br>
+
+<p>(Ezen felül egyszeri200 Ft a tagsági kis könyv kiállítási díja)<br>
+A tagsághoz szükséges a tagdíj befizetése, 2 igazolványkép és a kitöltött nyilatkozat, amelyet a Titkárság címére kell eljuttatni (1113 Bp. Nagyszőlős utca 21.)</p>
+
+
+<h2>Tárgyi adománnyal</h2>
+
+<p>Tárgyi adományokkal kapcsolatban, kérjük, a legközeebbi tagszervezetünkkel vegye fel a kapcsolatot, hogy megtudja, éppen mire szorulnak rá a legjobban.<br><br>
+
+Néhány dolog, amelyre szinte mindenhol szükség van:<img src="kutya3.jpg"  width="30%" align="right" ><br><br></p>
+
+    <ul>
+<li> állateledel minden mennyiségben
+<li> felszerelések
+    <ul>
+        <li> póráz, nyakörv
+         <li> kutya/macska ház
+              <li> itató-, etető tálak
+                   <li> alomanyagok
+                        <li>   játékok (labda, kötél)
+                            <li> tisztítószerek és felszerelések (pl. fertőtlenítőszerek)
+        <li>főleg tél előtt takarók, plédek, szőnyegek az állatok alá
+    gyógyszerek (bolha-, kullancsirtók, féreghajtók, kötszerek, fertőtlenítők)
+    </ul>
+    
+   
+
+        </ul>
+
+    
+
+<h2>Önkéntes munkával</h2>
+
+<p>Várjuk mindazok jelentkezését, akik aktívan is szeretnének tenni az állatvédelemért, azért, hogy a jelenlegi helyzet mihamarabb megváltozzon. Az önkéntesség feltétele a HEROSZ tagság. A önkéntes munkák igen sokrétűek, sokan nem is gondolnák, hogy mennyi mindenben tudnak a segítségünkre és ezáltal a bajban lévő állatok segítségére lenni (A szakosztályainkról bővebben az Önkéntessség menüpontban lehet olvasni). Kérjük, amennyiben szívesen csatlakozna hozzánk, vegye fel a Titkársággal vagy a legközelebbi tagszervezetünkkel a kapcsolatot!</p>
+<h2>Ideiglenes befogadással</h2>
+
+<p>Nagyon sokszor lenne szükségünk olyan befogadókra, akik véglegesen nem is, de ideiglenesen tudnának vállalni kutyát, macskát. Ez különösen nagy segítség az olyan tagszervezeteinknél, akik nem rendelkeznek állatotthonnal. Minél többen vállalnak ideiglenes befogadást, Egyesületünk annál több bajba jutott állaton tud segíteni.
+
+ <br><br>
+
+Különösen nagy szükség van ideiglenes befogadók: <img src="kutya2.jpg"  width="30%" align="right" > <br><br>
+
+   </p> <ul>
+<li> pár hetes kiskutyák/kiscicák gondozásánál
+<li>  lelkileg sérült állatok rehabilitációjánál
+<li>  télen rövidszőrű, kistestű, esetleg idős kutyáknál
+    <li> érzékeny állatoknál, akik lelkileg nem tudják elviselni az állatotthonokat és magukba fordulnak
+<li>   műtét után lábadozóknál
+</ul> 
+
+   
+
+   
+
+    
+   
+
+<h2>Virtuális örökbefogadással</h2>
+
+<p>Ha több állatot már nem tud befogadni, de szívesen támogatná egyik-másik gazdikeresőnket, akkor fogadjon örökbe virtuálisan (jelképesen) egyet! A virtuális örökbefogadással jelképesen nevére veszi az állatot, anélkül, hogy haza kellene vinnie. A virtuális örökbefogadó dolga, hogy megpróbáljon a kiválasztott állat számár megfelelő gazdát találni, amennyiben tud, eljöjjön megsétáltatni/megszeretgetni ill. havonta hozzájáruljon a tartási költségeihez. Virtuális örökbefogadásra az adott állat adatlapján van lehetőség!</p>
+ </form>
+    </body>
+</html>
