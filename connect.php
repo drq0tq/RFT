8cbdba9ff0f2017193eee6407158080810587709
birodaniella <biro.dana14@gmail.com>
2021-04-21 15:14:36 +0200

    Gondozók kilistázása DB-ből!

--- connect.php
+++ connect.php
@@ -1,9 +1,65 @@
-<?php
+
+<?php 
+define('DB_TYPE', 'mysql');
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'menhelydb');
+define('DB_CHARSET', 'utf8');
+
+function getConnection() {
+	$connection = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME.';',DB_USER, DB_PASS);
+	$connection->exec("SET NAMES '".DB_CHARSET."'");
+	return $connection;
+}
+
+function getRecord($queryString, $queryParams = []) {
+	$connection = getConnection();
+	$statement = $connection->prepare($queryString);
+	$success = $statement->execute($queryParams);
+	$result = $success ? $statement->fetch(PDO::FETCH_ASSOC) : [];
+	$statement->closeCursor();
+	$connection = null;
+	return $result;
+}
+
+function getList($queryString, $queryParams = []) {
+	$connection = getConnection();
+	$statement = $connection->prepare($queryString);
+	$success = $statement->execute($queryParams);
+	$result = $success ? $statement->fetchAll(PDO::FETCH_ASSOC) : [];
+	$statement->closeCursor();
+	$connection = null;
+	return $result;
+}
+
+function executeDML($queryString, $queryParams = []) {
+	$connection = getConnection();
+	$statement = $connection->prepare($queryString);
+	$success = $statement->execute($queryParams);
+	$statement->closeCursor();
+	$connection = null;
+	return $success;
+}
+
+function getField($queryString, $queryParams = []) {
+	$connection = getConnection();
+	$statement = $connection->prepare($queryString);
+	$success = $statement->execute($queryParams);
+	$result = $success ? $statement->fetch()[0]: [];
+	$statement->closeCursor();
+	$connection = null;
+	return $result;
+}
+
+
+
+
 //error_reporting(0);
-$db = new mysqli('127.0.0.1','root','','menhelydb');
+/*$db = new mysqli('127.0.0.1','root','','menhelydb');
 if($db->connect_errno){
 echo $db->connect_error;
 die();
-}
+}*/
 
 ?>
---
+++ listGondozok.php
@@ -0,0 +1,32 @@
+<?php
+include 'index.php';
+$query = "SELECT * FROM gondozok";
+require_once 'connect.php';
+$gondozok = getList($query);
+?>
+<?php if(count($gondozok)<=0) : ?>
+    <h1>Nem található gondozó a táblában</h1>
+<?php else : ?>
+<table>
+		<thead>
+			<tr>
+				<th scope="col">#</th>
+				<th scope="col">Név</th>
+				<th scope="col">Munkakör</th>
+				<th scope="col">Végzettség</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php $i = 0; ?>
+			<?php foreach ($gondozok as $g) : ?>
+				<?php $i++; ?>
+				<tr>
+					<th scope="row"><?=$i ?></th>
+					<td><?=$g['nev'] ?></td>
+					<td><?=$g['munkakor'] ?></td>
+					<td><?=$g['vegzettseg'] ?></td>
+				</tr>
+			<?php endforeach;?>
+		</tbody>
+	</table>
+<?php endif; ?>
