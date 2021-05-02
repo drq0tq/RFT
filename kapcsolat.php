1a0782002caadc0053aeb3016dcdb45f34cfe253
Bíborka <64154594+drq0tq@users.noreply.github.com>
2021-05-03 01:07:11 +0200

    kapcsolat oldal feltöltése

---
+++ kapcsolat.php
@@ -0,0 +1,56 @@
+<?php
+include 'index.php';
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+    #elerhetoseg{
+	margin-top: 80px;
+     color:#e65c00;
+    border-radius: 13px;
+    padding: 15px 15px 15px 15px;
+   border: 2px solid #990000;
+        background:#ffc266;
+        font-weight: bold;
+	
+	
+}
+#elerhetoseg td{
+	font-size:20px;
+	text-align:left;
+	padding: 0 15px 0 15px;
+    height: 100;
+    
+
+}
+#elerhetoseg th{
+	font-size:25px;
+	text-align:left;
+	padding: 0 15px 0 15px ;
+    height: 110px ;
+
+}
+#email:hover {
+        color: darkred;
+    }
+#email  {
+     color: orangered;
+    
+    }
+    hr {
+        color: #663300;
+    } 
+        
+    </style>
+  </head>
+  <body>
+	
+	<table id="elerhetoseg" align="center"  >
+	<tr><th rowspan="4"><iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2674.221556068294!2d20.34371541602662!3d47.912750379206145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47408dad44a27f5b%3A0x929b05c17f637c99!2sEger%2C%20F%C5%91%20utca%201%2C%203300!5e0!3m2!1shu!2shu!4v1618347109811!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></th><th colspan="2">Elérhetőségek:<hr></th></tr>
+<tr><td>Cím:</td><td>3300 Eger Fő utca 1</td></tr>
+<tr><td>Telefonszám:</td><td>+36302467542</td></tr>
+<tr><td>E-mail cím:</td><td><a  id ="email" href="mailto:nyuzokaaa@gmail.com"> nyuzokaaa@gmail.com </a></td></tr>
+</table>
+  </body>
+</html>
