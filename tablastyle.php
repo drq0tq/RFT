c66bd86e47dab4631d0d697c2d7df85cf43975fb
Bíborka <64154594+drq0tq@users.noreply.github.com>
2021-05-03 00:55:44 +0200

    táblastílus hozzáadása

---
+++ tablastyle.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+  #table tr, #table th, #table td{
+        border-bottom: 1px solid #e65c00;
+       padding: 10px;
+       text-align: center;
+  
+    }
+    #table
+    {
+     
+        font-family: system-ui;
+        width: 70%;
+    margin-left: auto;
+        margin-right: auto;
+        margin-top:  52px;
+        font-size: 25px;
+         
+         border-collapse: collapse;
+        
+    }
+#table tr:nth-child(even){background-color: #ffc34d}
+
+#table th{
+  background-color: #993300;
+  color: white;
+}
+        p{
+            margin-top:  45px;
+            text-align: center;
+            color:#e65c00;
+	font-weight: normal;
+font-size:30px;
+	font-family: cursive;
+		cursor:default;
+	
+}
+</style>
+</head>
+<body>
+    </body>
+</html>
