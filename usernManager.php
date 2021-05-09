6b1c61ce87b207b1ee59dcf48309e1dbe6fa0e87
Ádám <zorgadam@gmail.com>
2021-05-09 20:14:12 +0200

    usermanager register method

--- usernManager.php
+++ usernManager.php
@@ -27,6 +27,25 @@ function userLogin($email, $password){
     }
     return false;
 }
+function userRegister($email,$nev,$password, $kor){
+    $query = "SELECT id FROM felhasznalok WHERE email = :email";
+    $params = [':email' => $email];
 
+    require_once 'connect.php';
+    $record = getRecord($query,$params);
+    if(empty($record)){
+        $query = "INSERT INTO felhasznalok (nev,email,jelszo, eletkor) values (:nev, :email, :jelszo, :kor)";
+        $params = [
+            ':nev' => $nev,
+            ':email' => $email,
+            ':jelszo' => $password,
+            ':kor' => $kor
+        ];
+        if(executeDML($query,$params)){
+            header('Location: login.php');
+        }
+        return false;
+    }
+}
 
 ?>
