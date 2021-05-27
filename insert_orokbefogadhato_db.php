e563c8b0de7a1323a59b9f08d03f572b735f1dab
Ádám <zorgadam@gmail.com>
2021-05-27 23:40:47 +0200

    insert_orokbefogadhato_db.php

---
+++ insert_orokbefogadhato_db.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once 'connect.php';
+
+
+
+$nevErr= $kepzettsegErr =$korErr =$nemErr =$fajtaErr =$meretErr =$mozgasigenyErr =$ivartalanErr =$gyerekbaratErr =$munkakorErr="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    
+  if (empty($_POST["nev"])) {
+     
+  echo  $nevErr = "Név mező üres!";
+     
+      
+      
+  } else {
+   
+   $nev = ($_POST["nev"]);
+  
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$nev)) {
+    echo  $nevErr = "A Név mezőben hibás karakterek vannak!";
+          
+    } 
+      
+  }
+  
+   
+  if (empty($_POST["munkakor"])) {
+   echo $munkakorErr = "Munkakör mező üres!";
+     
+  } else {
+ $munkakor = ($_POST["munkakor"]);
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$munkakor)) {
+     echo $munkakorErr = "A munkakör mezőben hibás karakterek vannak";
+        
+    } 
+  }
+
+  if ("válasszon"=($_POST["vegzettseg"])) {
+      echo $vegzettsegErr = "Nem választott végzettséget!";
+  }
+    
+    
+    
+    
+}
+ if ( $besznevErr == "" && $beszemailErr == "" && $vegzettsegErr =="") {
+     
+     $sql = "insert into gondozok (nev,munkakor,vegzettseg) values ('$nev','$munkakor','$vegzettseg')";
+$conn->query($sql);
+include 'index.php';
+echo "Sikeres hozzáadás!"; 
+ } else { include 'index.php'; echo  "Sikertelen hozzáadás! $besznevErr $beszemailErr $vegzettsegErr"; }
+
+        ?>
